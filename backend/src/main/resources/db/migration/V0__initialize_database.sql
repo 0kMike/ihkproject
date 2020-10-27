4dadce7ce240d76611a6b5af35d5aa5f1b34e255
0kMike <m.naumann@windot.de>
2020-10-27 20:52:31 +0100

    add file for test data add lastdate in database migration file

--- backend/src/main/resources/db/migration/V0__initialize_database.sql
+++ backend/src/main/resources/db/migration/V0__initialize_database.sql
@@ -3,5 +3,6 @@ CREATE TABLE jobs
     jobkey          bigserial PRIMARY KEY,
     jobtitle        varchar(255) NOT NULL,
     expired         boolean NOT NULL,
-    location        bigint NOT NULL
+    location        bigint NOT NULL,
+    lastdate        date NOT NULL
 );
---
+++ backend/src/main/resources/db/migration/V10__test_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO jobs (jobkey, jobtitle, expired, location, lastdate)
+values (1337, 'Krankenpfleger', true, '76135', '2020-06-01');
