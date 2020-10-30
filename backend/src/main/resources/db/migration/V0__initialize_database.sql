24e738a1b8560b6d6d55ca34863a6adf1e6f4f62
0kMike <m.naumann@windot.de>
2020-10-30 23:54:17 +0100

    Rename job variables

--- backend/src/main/resources/db/migration/V0__initialize_database.sql
+++ backend/src/main/resources/db/migration/V0__initialize_database.sql
@@ -1,8 +1,8 @@
 CREATE TABLE job
 (
-    id              uuid PRIMARY KEY,
-    jobkey          bigint NOT NULL,
-    jobtitle        varchar(255) NOT NULL,
-    location        bigint NOT NULL,
-    lastdate        date NOT NULL
+    id       uuid PRIMARY KEY,
+    key      bigint       NOT NULL,
+    title    varchar(255) NOT NULL,
+    location bigint       NOT NULL,
+    lastdate date         NOT NULL
 );
