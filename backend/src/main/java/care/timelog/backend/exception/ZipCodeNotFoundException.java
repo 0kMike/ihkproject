aca0ef9543a23cdeddbd573a217a684944e2ee71
Michael Naumann <m.naumann@windot.de>
2020-11-05 02:02:48 +0100

    Add Annotation to custom exception to throw http 404 error

--- backend/src/main/java/care/timelog/backend/exception/ZipCodeNotFoundException.java
+++ backend/src/main/java/care/timelog/backend/exception/ZipCodeNotFoundException.java
@@ -1,5 +1,9 @@
 package care.timelog.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No location found for zipcode")
 public class ZipCodeNotFoundException extends Exception {
     public ZipCodeNotFoundException(String errorMessage) {
         super(errorMessage);
