90790809a5c63037ed87c11d45d8a31f94a50e9e
Michael Naumann <m.naumann@windot.de>
2020-11-09 17:03:29 +0100

    Make ZipCodeNotFoundException extend RuntimeException

--- backend/src/main/java/care/timelog/backend/exception/ZipCodeNotFoundException.java
+++ backend/src/main/java/care/timelog/backend/exception/ZipCodeNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No location found for zipcode")
-public class ZipCodeNotFoundException extends Exception {
+public class ZipCodeNotFoundException extends RuntimeException {
     public ZipCodeNotFoundException(String errorMessage) {
         super(errorMessage);
     }
