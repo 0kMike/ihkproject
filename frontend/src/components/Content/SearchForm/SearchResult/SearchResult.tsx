b19529145d225b6c8fc17eb13dd87dc7d02d756a
Michael Naumann <m.naumann@windot.de>
2020-11-15 17:49:44 +0100

    Add process for searchresult

--- frontend/src/components/Content/SearchForm/SearchResult/SearchResult.tsx
+++ frontend/src/components/Content/SearchForm/SearchResult/SearchResult.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './SearchResult.module.css';
+import {IJob} from "../../../../interfaces/IJob";
+import {generateSearchResultDisplay} from "../../../../provider/HistoryProvider";
+import {Line} from "react-chartjs-2";
+import {generateChart} from "../../../../provider/ChartProvider";
 
 interface IResultArea {
-
+  searchResult: IJob[];
 }
 
-function SearchResult(props: IResultArea) {
+const SearchResult = (props: IResultArea) => {
+  const {searchResult} = props;
+
+  const displayResult = generateSearchResultDisplay(searchResult);
+
+  const chartData = generateChart(displayResult)
+
   return (
     <section className={styles.resultArea}>
-      <div className={styles.timeLineGraph}/>
+      <div className={styles.history}>
+        {displayResult.length > 0 && <Line data={chartData} height={80}/>}
+      </div>
       <div className={styles.liveResults}/>
     </section>
   )
