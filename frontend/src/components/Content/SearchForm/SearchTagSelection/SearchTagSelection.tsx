170d2b4979e39554391bb9351eb09021872f74cd
Michael Naumann <m.naumann@windot.de>
2020-11-15 17:50:44 +0100

    rename jobs from JobProvider to searchableJobs

--- frontend/src/components/Content/SearchForm/SearchTagSelection/SearchTagSelection.tsx
+++ frontend/src/components/Content/SearchForm/SearchTagSelection/SearchTagSelection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './SearchTagSelection.module.css';
-import {jobs} from '../../../../provider/JobProvider';
+import {searchableJobs} from '../../../../provider/JobProvider';
 import SearchTag from './SearchTag/SearchTag';
 
 interface ISearchCriteria {
@@ -13,7 +13,7 @@ function SearchTagSelection(props: ISearchCriteria) {
   return (
     <div className={styles.seachTagSelection}>
       {
-        jobs.map((item, index) => {
+        searchableJobs.map((item, index) => {
           return (
             <SearchTag key={index} tag={item} isActive={props.activeSearchTags.some(element => {return element === item})} toggleActiveSearchTag={props.toggleActiveSearchTag}/>
           )
